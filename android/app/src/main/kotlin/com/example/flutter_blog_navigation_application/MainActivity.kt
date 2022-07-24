71330d23bb1578948ea4b49b46a5de1d30284706
RezaMahdinezhad <RezaMahdinezhad@gmail.com>
2022-07-24 16:10:42 +0430

    v1

---
+++ android/app/src/main/kotlin/com/example/flutter_blog_navigation_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_blog_navigation_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Text('salam'),
+        ),
+      ),
+    );
+  }
+}
